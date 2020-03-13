6e8ae56fdc15a8d5baca23d97f0c9769dc03d442
aiden <874735833@qq.com>
2020-03-13 22:37:38 +0800

    第一次上传本地库　

---
+++ test.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import glob2
+
+# 遍历文件夹中的txt文件
+FilePaths = glob2.glob(r'E:\研究生\毕业论文\toothdata\*.txt')  # 构造绝对路径，"\\"，其中一个'\'为转义符
+print(FilePaths)
+points= []
+
+
+# 初始化数组下标
+i = 0
+for txtPath in FilePaths: # 遍历txt
+    print(txtPath)
+    point = []
+    ++i
+    print(i)
+    with open(txtPath) as f:  # 打开文件
+        # 将txt文件中的数据存入数组
+        while 1:
+            line = f.readline()
+            if not line:
+                break
+            strs = line.split(" ")
+            point.append((float(strs[0]), float(strs[1]), float(strs[2]), float(strs[3]), float(strs[4]), float(strs[5])))
+        # points原本为列表，需要转变为矩阵，方便处理
+        point = np.array(point)
+    points[i].append(point)
+    print(points[i])
+points = np.array(points)
+print(points)
